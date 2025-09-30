334aa7b0c749918e8aaa6f5d9e941ed3a3d0ebf3
jaydixit01 <jayddixit1@gmail.com>
2025-09-29 20:54:26 -0500

    feat: implemented db operations, chunking

---
+++ custom_types.py
@@ -0,0 +1,21 @@
+import pydantic 
+from pydantic import BaseModel
+
+class RAGChunkAndSrc(BaseModel):
+    chunks: list[str]
+    source_id: str = None
+
+
+class RAGUpsertResult(BaseModel):
+    ingested: int
+
+
+class RAGSearchResult(BaseModel):
+    contexts: list[str]
+    sources: list[str]
+
+class RAGQueryResult(BaseModel):
+    answer: str
+    sources: list[str]
+    num_contexts: int
+    
